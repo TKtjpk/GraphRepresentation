c916cd9314f5a63542d3847d286d0c7a8c9794ce
Tomasz <80661109+TKtjpk@users.noreply.github.com>
2023-07-19 16:37:20 +0200

    Initial Commit

---
+++ GraphRepresentation/GraphRepresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphRepresentationApp.swift
+//  GraphRepresentation
+//
+//  Created by Tomasz Kubicki on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GraphRepresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
